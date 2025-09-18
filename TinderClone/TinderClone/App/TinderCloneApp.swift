e3824702030dca757d8a89e35cd054f162da5265
gabriellopes23 <lopessantosgabriel@outlook.com>
2025-09-18 08:43:32 -0300

    Setting Project

---
+++ TinderClone/TinderClone/App/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Gabriel Lopes on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinderClone/TinderClone/Core/TabBar/MainTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBar.swift
+//  TinderClone
+//
+//  Created by Gabriel Lopes on 18/09/25.
+//
+
+import SwiftUI
+
+struct MainTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabBar()
+}
